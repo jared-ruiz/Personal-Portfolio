2c5ef943fc453d532597e276a5b767d0bdfe5efd
Jared Ruiz <jared707@gmail.com>
2022-06-24 19:25:15 -0500

    Added resume to be downloaded upon button click.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Nav from './components/Nav'
 import About from './components/About'
 import Portfolio from './components/Portfolio'
 import Contact from './components/Contact'
+import Resume from './components/Resume'
 
 
 const App = () => {
@@ -35,6 +36,8 @@ const App = () => {
     if (currentCategory === categories[2]) {
       return <Contact/>
     }
+    return <Resume/>
+
     
 
   }
